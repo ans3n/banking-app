ba5609b09e6503e98448426d7a230ca8b8f32ab6
Ansen Tsan <139832422+ans3n@users.noreply.github.com>
2024-06-17 18:56:39 -0400

    implement credit cards on right bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,29 +1,37 @@
-import HeaderBox from '@/components/HeaderBox'
-import TotalBalanceBox from '@/components/TotalBalanceBox';
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
-  const loggedIn = { firstName: 'Ansen' };
+  const loggedIn = {
+    firstName: "Ansen",
+    lastName: "Shia",
+    email: "ansen616@gmail.com",
+  };
 
   return (
-    <section className='home'>
-      <div className='home-content'>
-        <header className='home-header'>
-          <HeaderBox 
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
             type="greeting"
             title="Welcome"
-            user={loggedIn?.firstName || 'Guest'}
+            user={loggedIn?.firstName || "Guest"}
             subtext="Access and manage your account or transactions efficiently."
           />
 
-          <TotalBalanceBox 
+          <TotalBalanceBox
             accounts={[]}
             totalBanks={1}
             totalCurrentBalance={1250.35}
           />
         </header>
+        RECENT TRANSACTIONS
       </div>
-    </section>
-  )
-}
 
-export default Home
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{currentBalance: 123.50}, {currentBalance: 500}]} />
+    </section>
+  );
+};
+
+export default Home;
