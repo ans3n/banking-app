8cbe19b147585da173baa5589a3f79fd6c52e23e
Ansen Tsan <139832422+ans3n@users.noreply.github.com>
2024-06-18 19:12:12 -0400

    Create appwrite sign up page

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,29 +1,45 @@
 "user server"
 
-import { createSessionClient } from "../appwrite";
+import { ID } from "node-appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
+import { cookies } from "next/headers";
+import { parseStringify } from "../utils";
 
 export const signIn = async () => {
     try {
         //mutation or database or fetch
-    } catch(error) {
+    } catch (error) {
         console.error('Error', error);
     }
 }
 
 export const signUp = async (userData: SignUpParams) => {
+    //destructuring syntax
+    const {email, password, firstName, lastName} = userData;
     try {
-        // create a user account
-    } catch(error) {
+        const { account } = await createAdminClient();
+
+        const newUserAccount = await account.create(ID.unique(), email, password, `${firstName} ${lastName}`);
+        const session = await account.createEmailPasswordSession(email, password);
+
+        cookies().set("appwrite-session", session.secret, {
+            path: "/",
+            httpOnly: true,
+            sameSite: "strict",
+            secure: true,
+        });
+
+        return parseStringify(newUserAccount); //nextjs cannot pass large objects(newuseraccount) through server actions - must stringify first
+    } catch (error) {
         console.error('Error', error);
     }
 }
 
 export async function getLoggedInUser() {
     try {
-      const { account } = await createSessionClient();
-      return await account.get();
+        const { account } = await createSessionClient();
+        return await account.get();
     } catch (error) {
-      return null;
+        return null;
     }
-  }
-  
+}
