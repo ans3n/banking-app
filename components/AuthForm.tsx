4ae63b044625baf0d999111a9bfd2e653dd08d12
Ansen Tsan <139832422+ans3n@users.noreply.github.com>
2024-06-18 18:52:44 -0400

    additional signup/signin frontend

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -20,11 +20,13 @@ import { Input } from "@/components/ui/input";
 import CustomInput from "./CustomInput";
 import { authFormSchema } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 {
   /* uses shadcn React hook forms - https://ui.shadcn.com/docs/components/form*/
 }
 const AuthForm = ({ type }: { type: string }) => {
+  const router = useRouter();
   const [user, setUser] = useState(null);
   const [isLoading, setisLoading] = useState(false);
 
@@ -40,13 +42,34 @@ const AuthForm = ({ type }: { type: string }) => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
     setisLoading(true);
-    console.log(values);
-    setisLoading(false);
-  }
+
+    try {
+      //sign up with Appwrite and create plaid token
+
+      if (type === "sign-up") {
+        const newUser = await signUp(data);
+        setUser(newUser);
+      }
+
+      if (type === "sign-in") {
+        
+        const response = await signIn({
+          email: data.email,
+          password: data.password,
+        })
+        if (response) {
+          router.push('/');
+        }
+      }
+      
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setisLoading(false);
+    }
+  };
 
   return (
     <section className="auth-form">
@@ -103,6 +126,13 @@ const AuthForm = ({ type }: { type: string }) => {
                     name="address1"
                     label="Address"
                     placeholder="Enter your specific address"
+                  />
+
+                  <CustomInput
+                    control={form.control}
+                    name="city"
+                    label="City"
+                    placeholder="Enter your city"
                   />
 
                   <div className="flex gap-4">
---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,17 @@
+"user server"
+
+export const signIn = async () => {
+    try {
+        //mutation or database or fetch
+    } catch(error) {
+        console.error('Error', error);
+    }
+}
+
+export const signUp = async () => {
+    try {
+        //mutation or database or fetch
+    } catch(error) {
+        console.error('Error', error);
+    }
+}
